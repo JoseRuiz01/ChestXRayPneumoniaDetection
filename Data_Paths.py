2e69b0fdab376f0bf24c0c2e2eb1ccc712ac1bee
José Antonio Ruiz Heredia <joseruizheredia01@gmail.com>
2024-03-14 15:35:31 +0100

    Separado etiquetado de dataset en otro jupyter y creado archivo con rutas
    Si se modifican las rutas de Data_paths, al hacer commit ignorar o descartar ese cambio para que no nos estemos cambiando las rutas entre nosotros.

---
+++ Data_Paths.py
@@ -0,0 +1,14 @@
+# ------------------------------------------------------#
+
+# Rutas de datos específicas de nuestro ordenador.
+#Cambiar la ruta dependiendo de donde tengas los datos.
+
+# ------------------------------------------------------#
+
+# Dataset
+TRAIN_DATASET = "C:\src\Personal\SI\Chest_X_Ray_Pneumonia_Detection\Dataset\Train"
+TEST_DATASET = "C:\src\Personal\SI\Chest_X_Ray_Pneumonia_Detection\Dataset\Test"
+VAL_DATASET = "C:\src\Personal\SI\Chest_X_Ray_Pneumonia_Detection\Dataset\Val"
+
+# CSV
+CSV_DATSET = "C:\src\Personal\SI\Chest_X_Ray_Pneumonia_Detection\Dataset"
